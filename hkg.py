ecd030f6b37c2e521cacbc431d22498acae70783
alfredtruong <alfred.truong@gmail.com>
2024-06-11 15:43:18 +0800

    testing before multithreading

--- hkg.py
+++ hkg.py
@@ -16,13 +16,13 @@ chromedriver_autoinstaller.install()
 
 
 def chrome_driver_options():
-	my_proxy = random.choice(proxies_list)
-	my_user_agent = random.choice(user_agents)
-	chrome_options = Options()
-	chrome_options.add_argument("--headless=new")
-	chrome_options.add_argument(f'--proxy-server={my_proxy}')
-	chrome_options.add_argument(f"--user-agent={my_user_agent}")
-	return chrome_options
+    my_proxy = random.choice(proxies_list)
+    my_user_agent = random.choice(user_agents)
+    chrome_options = Options()
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument(f'--proxy-server={my_proxy}')
+    chrome_options.add_argument(f"--user-agent={my_user_agent}")
+    return chrome_options
 
 #%%
 
@@ -33,61 +33,61 @@ from scrape_params import user_agents,proxies_list
 IS_DEV = True
 
 def captureData(thread_id: int):
-	browser = webdriver.Chrome(options=chrome_driver_options())
-	if IS_DEV:
-		browser.maximize_window()
+    browser = webdriver.Chrome(options=chrome_driver_options())
+    if IS_DEV:
+        browser.maximize_window()
 
-	################### main page
-	# load
-	page = f"http://lihkg.com/thread/{thread_id}"
-	print(page)
-	browser.get(page)
-	timeout = 10
-	try:
-		element_present = EC.presence_of_element_located((By.CLASS_NAME, '_36ZEkSvpdj_igmog0nluzh'))
-		WebDriverWait(browser, timeout).until(element_present)
-	except TimeoutException:
-		print ("Timed out waiting for page to load" )
-		return None
+    ################### main page
+    # load
+    page = f"http://lihkg.com/thread/{thread_id}"
+    print(page)
+    browser.get(page)
+    timeout = 10
+    try:
+        element_present = EC.presence_of_element_located((By.CLASS_NAME, '_36ZEkSvpdj_igmog0nluzh'))
+        WebDriverWait(browser, timeout).until(element_present)
+    except TimeoutException:
+        print ("Timed out waiting for page to load" )
+        return None
 
-	# identify total number of subpages for this thread
-	content = browser.find_element(By.CLASS_NAME,'_1H7LRkyaZfWThykmNIYwpH') # get page container
-	opt = content.find_elements(By.TAG_NAME,'option') # get page options (figure out how many pages of info)
-	print(f'opt = {opt}')
-	print(f'len(opt) = {len(opt)}')
-	
-	with io.open(f"{thread_id}Exported.txt", "w", encoding="utf-8") as file:
-		################### subpage
-		for counter in range(1, len(opt)):
-			# load
-			subpage = f"http://lihkg.com/thread/{thread_id}/page/{counter}"
-			print('\t',subpage)
-			browser.get(subpage)
-			timeout = 10
-			try:
-				element_present = EC.presence_of_element_located((By.CLASS_NAME, '_2cNsJna0_hV8tdMj3X6_gJ'))
-				WebDriverWait(browser, timeout).until(element_present)
-			except TimeoutException:
-				file.write(f"Timed out waiting for page {counter} to load")
-				return None
+    # identify total number of subpages for this thread
+    content = browser.find_element(By.CLASS_NAME,'_1H7LRkyaZfWThykmNIYwpH') # get page container
+    opt = content.find_elements(By.TAG_NAME,'option') # get page options (figure out how many pages of info)
+    print(f'opt = {opt}')
+    print(f'len(opt) = {len(opt)}')
+    
+    with io.open(f"{thread_id}Exported.txt", "w", encoding="utf-8") as file:
+        ################### subpage
+        for counter in range(1, len(opt)):
+            # load
+            subpage = f"http://lihkg.com/thread/{thread_id}/page/{counter}"
+            print('\t',subpage)
+            browser.get(subpage)
+            timeout = 10
+            try:
+                element_present = EC.presence_of_element_located((By.CLASS_NAME, '_2cNsJna0_hV8tdMj3X6_gJ'))
+                WebDriverWait(browser, timeout).until(element_present)
+            except TimeoutException:
+                file.write(f"Timed out waiting for page {counter} to load")
+                return None
 
-		# make sure page loaded
-		content = browser.find_elements(By.CLASS_NAME,'_36ZEkSvpdj_igmog0nluzh')
-		for x in content:
-			file.write("*************************************************")
-			file.write(x.text)
+        # make sure page loaded
+        content = browser.find_elements(By.CLASS_NAME,'_36ZEkSvpdj_igmog0nluzh')
+        for x in content:
+            file.write("*************************************************")
+            file.write(x.text)
 
-	# close output file
-	file.close()
+    # close output file
+    file.close()
 
-	# close chromedriver
-	if browser is not None:
-		browser.quit()
+    # close chromedriver
+    if browser is not None:
+        browser.quit()
 #%%
 
 if(len(sys.argv)<2):
-	print("no post ID found")
-	sys.exit()
+    print("no post ID found")
+    sys.exit()
 
 print("***simple scrape lihkg ***")
 print("Version "+__version__)
--- scrape_threads.py
+++ scrape_threads.py
@@ -1,8 +1,7 @@
 #%%
 
-import sys
 import os
-import io
+import sys
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import TimeoutException
@@ -20,150 +19,295 @@ chromedriver_autoinstaller.install()
 import threading
 lock = threading.Lock()
 
-# init chromedriver
+############### chromedriver
+# init
 def get_browser():
-	if TESTING:
-		browser = webdriver.Chrome()
-		browser.maximize_window()
-	else:
-		chrome_options = Options()
-		chrome_options.add_argument("--headless=new")
-		chrome_options.add_argument(f'--proxy-server={random.choice(proxies_list)}')
-		chrome_options.add_argument(f"--user-agent={random.choice(user_agents)}")
-		browser = webdriver.Chrome(options=chrome_options)
-	return browser
+    if IS_TESTING:
+        browser = webdriver.Chrome()
+        browser.maximize_window()
+    else:
+        chrome_options = Options()
+        chrome_options.add_argument("--headless=new")
+        chrome_options.add_argument("--disable-popup-blocking")
+        chrome_options.add_argument(f'--proxy-server={random.choice(proxies_list)}')
+        chrome_options.add_argument(f"--user-agent={random.choice(user_agents)}")
+        browser = webdriver.Chrome(options=chrome_options)
+    return browser
 
-def add_subpage_comments_to_jsonl(url: str, topic: str, title: str, comments: List[str]):
-	# generic dico for this 
-	base_dico = {
-		'url' : url,
-		'topic' : topic,
-		'title' : title,
-	}
-	with lock:
-		with open(OUTPUT_FILE, 'a', encoding='utf-8') as f:
-			for comment in comments:
-				base_dico['comment'] = comment
-				line = json.dumps(base_dico, ensure_ascii=False)
-				f.write(line + '\n')
+############### json utils
+# read file
+def read_json(filepath: str):
+    # load
+    try:
+        with open(filepath, 'r') as f:
+            data = json.load(f)
+    except Exception as e:
+        print(e)
+        data = {}
+    return data
 
+# save file
+def write_json(filepath: str, dico: Dict[str,object]):
+    # overwrite
+    with open(filepath, 'w') as f:
+        json.dump(dico, f)
+
+############### thread status file
+# log failure to load
+def thread_load_failure(thread_url: str, thread_id: int):
+    with lock:
+        # read dico from file
+        thread_status_dico = read_json(THREAD_STATUS_JSON)
+
+        # edit dico
+        if thread_url in thread_status_dico:
+            # existing entry
+            thread_status_dico[thread_url]['attempts'] += 1
+        else:
+            # new entry
+            thread_status_dico[thread_url] = {
+                'id': thread_id,
+                'attempts': 1,
+            }
+
+        # overwrite file
+        write_json(THREAD_STATUS_JSON, thread_status_dico)
+
+# log handled subpage ids
+def update_thread_handled_subpage_ids(thread_url: str, thread_id: int, handled_threads: List[int]):
+    with lock:
+        # read dico from file
+        thread_status_dico = read_json(THREAD_STATUS_JSON)
+
+        # edit dico
+        if thread_url in thread_status_dico:
+            # existing entry
+            thread_status_dico[thread_url]['attempts'] += 1
+            thread_status_dico[thread_url]['handled_subpage_ids'] = handled_threads
+        else:
+            # new entry
+            thread_status_dico[thread_url] = {
+                'id': thread_id,
+                'attempts': 1,
+                'handled_subpage_ids': handled_threads,
+            }
+
+        # overwrite file
+        write_json(THREAD_STATUS_JSON, thread_status_dico)
+
+############### thread comments file
+# add scraped comments
+def write_comments_to_jsonl(url: str, topic: str, title: str, comments: List[str]):
+    # check if file exists, create directory if it doesn't
+    dir_path = os.path.dirname(THREAD_COMMENTS_JSONL)
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+    with lock:
+        # generic dico to reuse for each comment
+        generic_dico = {
+            'url' : url, # subpage url
+            'topic' : topic, # thread topic
+            'title' : title, # thread title
+            'idx': None, # comment counter within page
+            'text' : None, # comment text
+        }
+        # write line for each comment
+        with open(THREAD_COMMENTS_JSONL, 'a', encoding='utf-8') as f:
+            for i,comment in enumerate(comments):
+                generic_dico['idx'] = i # overwrite comment
+                generic_dico['text'] = comment # overwrite comment
+                #print(f'[write_comments_to_jsonl] {generic_dico}')
+                line = json.dumps(generic_dico, ensure_ascii=False) # build line
+                #print(f'[write_comments_to_jsonl] {line}')
+                f.write(line + '\n') # write line
+
+
+'''
 def capture_subpage_success(filepath: str, url: str):
     with lock:
         with open(filepath, 'a') as f:
             f.write(url + '\n')
 
 def remove_url_from_txt_file(filepath: str, url: str):
-	# create tmp file
-	tmp_filepath = filepath.replace('.txt','_tmp.txt')
-	with open(filepath, 'r') as f1:
-		with open(tmp_filepath, 'w') as f2:
-			for line in f1:
-				if line != url:
-					f2.write(line + '\n')
-	
-	# overwrite existing file
-	os.rename(tmp_filepath,filepath)
-
-3706516
-# capture comments in thread subpage
-def capture_subpage(browser: webdriver.Chrome, subpage_url) -> int:
-	# load
-	try:
-		browser.get(subpage_url)
-		print('\t',subpage_url)
-		element_present = EC.presence_of_element_located((By.CLASS_NAME, '_2cNsJna0_hV8tdMj3X6_gJ'))
-		WebDriverWait(browser, TIMEOUT).until(element_present)
-		thread_topic = browser.find_element(By.CLASS_NAME,'_3V1lN0nDwZVDoc14rz7D8w').text,
-		thread_title = browser.find_element(By.CLASS_NAME,'_2k_IfadJWjcLJlSKkz_R2-').text, # this probably doesnt work
-		comments = [x.text for x in browser.find_elements(By.CLASS_NAME,'_36ZEkSvpdj_igmog0nluzh')],
-		add_subpage_comments_to_jsonl(subpage_url,thread_topic,thread_title,comments)
-		return 1 # success for this subpage
-		add_url_to_txt_file(SUBPAGE_SUCCESS,subpage_url)
-	except TimeoutException:
-		add_url_to_txt_file(SUBPAGE_FAILURE,subpage_url)
-		return 0 # failure for this subpage
-
-def load_thread_failure(thread_url: str):
-	pass
+    # create tmp file
+    tmp_filepath = filepath.replace('.txt','_tmp.txt')
+    with open(filepath, 'r') as f1:
+        with open(tmp_filepath, 'w') as f2:
+            for line in f1:
+                if line != url:
+                    f2.write(line + '\n')
+    
+    # overwrite existing file
+    os.rename(tmp_filepath,filepath)
 
 def capture_thread_success(thread_url: str):
-	pass
+    with lock:
+        with open(THREAD_SUCCESS_JSON, 'a') as f:
+            f.write(thread_url + '\n')
 
+'''
+#3706516
+
+# capture comments in thread subpage
+def capture_thread_subpage(
+    browser: webdriver.Chrome,
+    thread_id: int,
+    subpage_id: int,
+) -> bool:
+    # thread_id,subpage_id=1,1
+    try:
+        # generate url
+        thread_subpage_url = thread_id_and_subpage_id_to_url(thread_id,subpage_id)
+        
+        # load webpage
+        browser.get(thread_subpage_url)
+        if IS_TESTING: print(f'[capture_thread_subpage] {thread_id} {subpage_id}')
+        element_present = EC.presence_of_element_located((By.CLASS_NAME, '_2cNsJna0_hV8tdMj3X6_gJ'))
+        WebDriverWait(browser, WEBDRIVER_TIMEOUT).until(element_present)
+
+        # get topic title
+        thread_topic_text = browser.find_element(By.CLASS_NAME,'_3V1lN0nDwZVDoc14rz7D8w').find_element(By.TAG_NAME,'span').text
+
+        # get thread title
+        thread_title_text = browser.find_element(By.CLASS_NAME,'CrheYfsiQFY-vLMnO378W').text
+
+        # get comments
+        #comments = browser.find_element(By.CLASS_NAME,'eslltWt3HgKnG_miBwvfx').find_elements(By.CLASS_NAME,'_2cNsJna0_hV8tdMj3X6_gJ')
+        comments = browser.find_element(By.CLASS_NAME,'eslltWt3HgKnG_miBwvfx').find_elements(By.CLASS_NAME,'GAagiRXJU88Nul1M7Ai0H')
+        comments_text = [x.text for x in comments]
+
+        # write to file
+        write_comments_to_jsonl(thread_subpage_url,thread_topic_text,thread_title_text,comments_text)
+        return True # success
+    except TimeoutException:
+        return False # failure
+
+# generate thread url
+def thread_id_to_url(thread_id: int) -> str:
+    return f"http://lihkg.com/thread/{thread_id}"
+
+# generate subpage url
+def thread_id_and_subpage_id_to_url(thread_id: int, subpage_id: int) -> str:
+    return f"http://lihkg.com/thread/{thread_id}/page/{subpage_id}"
 
 # capture entire thread
-def capture_thread(browser: webdriver.Chrome, thread_id: int) -> int:
-	################### main page
-	thread_url = f"http://lihkg.com/thread/{thread_id}"
-	print(thread_url)
-	try:
-		browser.get(thread_url)
-		element_present = EC.presence_of_element_located((By.CLASS_NAME, '_36ZEkSvpdj_igmog0nluzh'))
-		WebDriverWait(browser, TIMEOUT).until(element_present)
-	except TimeoutException:
-		################### capture_thread failure
-		load_thread_failure(thread_url)
-		return 0
+def capture_thread(
+    browser: webdriver.Chrome,
+    thread_id: int,
+    skip_partial_thread_successes: bool = False, # should ignore threads with partial successes
+) -> int:
+    '''
+    thread_id=4
+    '''
+    ################### ref data
+    thread_url = thread_id_to_url(thread_id) # thread_url
+    handled_threads = read_json(THREAD_STATUS_JSON) # load previously handled threads
+    
+    ################### should revisit previously handled threads?
+    if skip_partial_thread_successes:
+        # dont revisit thread if something previously saved
+        if thread_url in handled_threads:
+            if IS_TESTING: print(f'[capture_thread][{thread_url}] skip, already visited')
+            return
 
-	################### identify total subpages
-	content = browser.find_element(By.CLASS_NAME,'_1H7LRkyaZfWThykmNIYwpH') # get page container
-	opt = content.find_elements(By.TAG_NAME,'option') # get page options (figure out how many pages of info)
-	pages = len(opt)
+    ################### go through uncaptured subpages captured
+    try:
+        # load main page
+        browser.get(thread_url)
+        element_present = EC.presence_of_element_located((By.CLASS_NAME, '_36ZEkSvpdj_igmog0nluzh'))
+        WebDriverWait(browser, WEBDRIVER_TIMEOUT).until(element_present)
 
-	################### subpage
-	subpage_successes = 0
-	for subpage in range(1, len(opt)+1):
-		subpage_url = f"http://lihkg.com/thread/{thread_id}/page/{subpage}"
-		subpage_successes += capture_subpage(browser, subpage_url) # count successes
+        # identify count of subpages
+        content = browser.find_element(By.CLASS_NAME,'_1H7LRkyaZfWThykmNIYwpH') # get page container
+        options = content.find_elements(By.TAG_NAME,'option') # get page options (figure out how many pages of info)
+        total_subpages = len(options)
 
-	################### subpage
-	if pages == subpage_successes:
-		capture_thread_success(thread_url)
-		return 1 # total success
-	else:
-		capture_thread_failure(thread_url)
-		return 0 # something failed
+        # identify subpages to ignore (there maybe new subpages / new comments)
+        #print(handled_threads)
+        thread_details = handled_threads.get(thread_url,{})
+        #print(thread_details)
+        handled_subpage_ids = thread_details.get('handled_subpage_ids',[])
+        #print(handled_subpage_ids)
+        if IS_TESTING: print(f'[capture_thread][{thread_url}] handled_subpage_ids = {handled_subpage_ids}')
 
-#%%
+        # capture unhandled pages
+        newly_handled_subpage_ids = []
+        for subpage_id in range(1,total_subpages+1):
+            if subpage_id in handled_subpage_ids:
+                # ignore
+                if IS_TESTING: print(f'[capture_thread][{thread_url}] ignore')
+            else:
+                # handle
+                if IS_TESTING: print(f'[capture_thread][{thread_url}] handle')
+                capture_res = capture_thread_subpage(browser, thread_id,subpage_id)
+                if capture_res:
+                    newly_handled_subpage_ids.append(subpage_id)
+        
+        # batch updated newly handled subpages
+        handled_threads = handled_subpage_ids + newly_handled_subpage_ids
+        update_thread_handled_subpage_ids(thread_url,thread_id,handled_threads)
 
-# close chromedriver
-if browser is not None:
-	browser.quit()
+    except TimeoutException:
+        ################### capture_thread failure
+        thread_load_failure(thread_url,thread_id)
+        return
 
-################################# PARAMS
+################################# SCRAPE PARAMS
 from scrape_params import user_agents,proxies_list
 
-################################# SETTINGS
-OUTPUT_FILE = 'asset/scrape/'
-
-RANGE_FROM = 1
-RANGE_TO = 10
-RANGE_FILENAME_PREFIX = 'lihkg_{RANGE_FROM}_{RANGE_TO}'
-THREAD_SUCCESS = 'assets/scrapes/{RANGE_FILENAME_PREFIX}_thread_success.txt'
-THREAD_FAILURE = 'assets/scrapes/{RANGE_FILENAME_PREFIX}_thread_failure.txt'
-SUBPAGE_SUCCESS = 'assets/scrapes/{RANGE_FILENAME_PREFIX}_subpage_success.txt'
-SUBPAGE_FAILURE = 'assets/scrapes/{RANGE_FILENAME_PREFIX}_subpage_failure.txt'
-OUTPUT_FILE = 'assets/scrapes/{RANGE_FILENAME_PREFIX}.jsonl'
-TESTING = True
-TIMEOUT = 10
-TEST_MAX_URLS = 150
+################################# RUN SETTINGS
+IS_TESTING = False
 WORKERS = 50
+THREAD_FROM = 1
+THREAD_TO = 10
+OUTPUT_FILE_PREFIX = f'lihkg_{THREAD_FROM}_{THREAD_TO}'
+THREAD_STATUS_JSON = f'assets/scrapes/{OUTPUT_FILE_PREFIX}_status.json'
+THREAD_COMMENTS_JSONL = f'assets/scrapes/{OUTPUT_FILE_PREFIX}.jsonl'
+WEBDRIVER_TIMEOUT = 10
+SKIP_VISITED_THREADS = True
 WAIT_MIN_SHORT, WAIT_MAX_SHORT = 5, 10
 WAIT_MIN_LONG, WAIT_MAX_LONG = 60, 120
 
-
+#%%
+################################# START
 browser = get_browser()
-capture_thread(browser,1)
+
+#%%
+capture_thread(browser,1,SKIP_VISITED_THREADS)
+capture_thread(browser,2,SKIP_VISITED_THREADS)
+capture_thread(browser,3,SKIP_VISITED_THREADS)
+capture_thread(browser,4,SKIP_VISITED_THREADS)
+#capture_thread(browser,3716261,SKIP_VISITED_THREADS)
+
+#capture_thread_subpage(browser,3716261,1)
+#%%
+
+if browser is not None:
+    browser.quit()
+
 #%%
 
 if(len(sys.argv)<2):
-	print("no post ID found")
-	sys.exit()
+    print("no post ID found")
+    sys.exit()
 
 print("***simple scrape lihkg ***")
-capture_thread(sys.argv[1])
 print("Finished")
 
 # %%
 
 
 # capture_thread(1)
+
+
+# close chromedriver
+
+#%%
+capture_thread_subpage(browser,4,1)
+capture_thread(browser,4)
+
+# %%
+
+
+
